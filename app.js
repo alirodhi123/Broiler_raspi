04db15f8adf535c0ad89babcf82649fbd400c2d9
alirodhi123 <alirodhi123@gmail.com>
2018-06-04 00:32:50 +0700

    update terbaru

--- app.js
+++ app.js
@@ -29,13 +29,15 @@ var RELAY_SPRAY = BASE_URL+"relay/update-relay/spray";
 var RELAY_EXHAUST = BASE_URL+"relay/update-relay/exhaust";
 var BASE_RELAY_STATE = BASE_URL+"relay/get-relay/state";
 
+//setup relay state
+getRelayState(BASE_RELAY_STATE)
 
 sc.on('relay1', (data) => {
   if(data.status){
    RELAY1.writeSync(0);
    console.log('Relay Lamp: ', data.status);
    updateRelay(RELAY_LAMP, true);
-   createLogActivity(LOG_LAMP, 'Lamp Notification', 'Lamp is already running');
+   createLogActivity(LOG_LAMP, 'Lamp Notification', 'Lamp is turned on');
   }
   else{
    RELAY1.writeSync(1);
@@ -45,9 +47,59 @@ sc.on('relay1', (data) => {
   }
 })
 
+sc.on('relay2', (data) => {
+  if(data.status){
+   RELAY2.writeSync(0);
+   console.log('Relay Fan: ', data.status);
+   updateRelay(RELAY_FAN, false);
+   createLogActivity(LOG_FAN, 'Fan Notification', 'Fan is turned on'); 
+  }
+  else{
+   RELAY2.writeSync(1);
+   console.log('Relay Fan: ', data.status);
+   updateRelay(RELAY_FAN, false);
+   createLogActivity(LOG_FAN, 'Fan Notofication', 'Fan is turned off');
+  }
+})
+
+sc.on('relay3', (data) => {
+  if(data.status){
+   RELAY3.writeSync(0);
+   console.log('Relay Spray: ', data.status);
+   updateRelay(RELAY_SPRAY, true);
+   createLogActivity(LOG_SPRAY, 'Spray Notification', 'Spray is turned on');
+  }
+  else{
+   RELAY3.writeSync(1);
+   console.log('Relay Spray: ', data.status);
+   updateRelay(RELAY_SPRAY, false);
+   createLogActivity(LOG_SPRAY, 'Spray Notification', 'Spray is turned off');
+  }
+})
+
+sc.on('relay4', (data) => {
+  if(data.status){
+   RELAY4.writeSync(0);
+   console.log('Relay Exhaust: ', data.status);
+   updateRelay(RELAY_EXHAUST, true);
+   createLogActivity(LOG_EXHAUST, 'Exhaust Notification', 'Exhaust is turned on');
+  }
+  else{
+   RELAY4.writeSync(1);
+   console.log('Relay Exhaust: ', data.status);
+   updateRelay(RELAY_EXHAUST, false);
+   createLogActivity(LOG_EXHAUST, 'Exhaust Notification', 'Exhaust is turned off');
+  }
+})
+
 function updateRelay(url, status){
   var args = {
-   data: { state: status }
+   data: {
+     state: status
+   },
+   headers: {
+     "Content-Type": "application/json"
+   }
   }
 
   client.post(url, args, function(data, response){
@@ -57,12 +109,56 @@ function updateRelay(url, status){
 
 function createLogActivity(url, title, message){
   var args = {
-   data: { title: title, detail: message}
+   data: {
+     title: title,
+     keterangan: message
+   },
+   headers: {
+     "Content-Type": "application/json"
+   }
   }
 
   client.post(url, args, function(data, response){
   // console.log(response)
-   console.log('Berhasil'+ message)
+   console.log(message)
   })
+}
 
+function getRelayState(url){
+  var args = {
+   headers: {
+    "Content-Type": "application/json"
+   }
+  }
+
+  client.get(url, args, function(data, response){
+   console.log("State relay data ", data)
+   if(data.lamp){
+    RELAY1.writeSync(1);
+   } 
+   else{
+    RELAY1.writeSync(0);
+   }
+
+   if(data.fan){
+    RELAY2.writeSync(1);
+   }
+   else{
+    RELAY2.writeSync(0);
+   }
+
+   if(data.spray){
+    RELAY3.writeSync(1);
+   }
+   else{
+    RELAY3.writeSync(0);
+   }
+
+   if(data.exhaust){
+    RELAY4.writeSync(1);
+   }
+   else{
+    RELAY4.writeSync(0);
+   }
+  })
 }
